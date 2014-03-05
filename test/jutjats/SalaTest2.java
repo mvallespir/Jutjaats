e654b38dc47ef48f7bf8fa6f43f42c2363ebe4c6
mvallespir <MVC@192.168.7.188>
2014-03-05 15:36:55 +0100

    SalaTest: Test de treure un llistat de les dates ocupades de la Sala

--- test/jutjats/SalaTest2.java
+++ test/jutjats/SalaTest2.java
@@ -4,7 +4,9 @@
  */
 package jutjats;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -29,7 +31,7 @@ public class SalaTest2 {
 
     /**
      * Test of afegirJudici method, of class Sala.
-     * 
+     *
      * Que ficant un judici ens retorni true de que ha anat be insertarlo.
      */
     @Test
@@ -46,8 +48,8 @@ public class SalaTest2 {
     /**
      * Test of treureJudici method, of class Sala.
      *
-     * Es vol provar que ficant un nombre superior als objectes que te el llistat
-     * no falla.
+     * Es vol provar que ficant un nombre superior als objectes que te el
+     * llistat no falla.
      *
      */
     @Test
@@ -60,25 +62,38 @@ public class SalaTest2 {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
     }
-    
+
     /**
      * Test of datesOcupades method, of class Sala.
      *
-     *Test per comprovar el funcionament correcte de treure un llistat de dates ocupades.
-     * Desde  web
+     * Test per comprovar el funcionament correcte de treure un llistat de dates
+     * ocupades. Desde web
      */
     @Test
-    public void testDatesOcupades(){
+    public void testDatesOcupades() {
         System.out.println("\ntreureDatesSalaOcupade");
         Sala instance = new Sala(1, 20);
         Judici casNos = new Judici();
         Judici casColacao = new Judici();
+
+        casNos.afegirDate("01-03-2014");
+        casNos.afegirDate("02-03-2014");
+        casNos.afegirDate("04-03-2014");
+        casColacao.afegirDate("27-02-2014");
+        casColacao.afegirDate("03-03-2014");
+        casColacao.afegirDate("05-03-2014");
+        
         
         instance.afegirJudici(casNos);
         instance.afegirJudici(casColacao);
-        
-        
-        
+
+        ArrayList<Date> datesLlista = instance.datesOcupade();
+
+        for (Date d : datesLlista) {
+            String format = "dd-MM-yyyy";
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            System.out.println(sdf.format(d));
+        }
+
     }
-    
 }
