10a5476dfb375bd66fa136040538b79be5698e41
Swing <Swing@SWING-LAPTOP>
2014-03-05 17:39:58 +0100

    web prova

--- src/jutjats/Delicte.java
+++ src/jutjats/Delicte.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package jutjats;
 
 import java.text.ParseException;
@@ -12,7 +8,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author MVC
+ * @author MVC i RSM
  */
 public class Delicte {
 
