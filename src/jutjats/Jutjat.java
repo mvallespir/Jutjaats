2c9d95a66312d1854cea9828a3a19c0faf07ec1d
mvallespir <vallespirmiquel@gmail.com>
2014-03-19 16:32:32 +0100

    Objecte Jutjat comentat per a JavaDoc i format a la pagina.

--- src/jutjats/Jutjat.java
+++ src/jutjats/Jutjat.java
@@ -1,21 +1,46 @@
 package jutjats;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 /**
+ * Classe per dur la gestio dels jutjats.
  *
  * @author MVC i RSM
  */
 public class Jutjat {
 
+    /**
+     * Atribut nom del Jutjat.
+     */
     private String nom;
+    /**
+     * Atribut direcció del Jutjat.
+     */
     private String direccio;
+    /**
+     * Atribut poblacio on es troba el Jutjat.
+     */
     private String poblacio;
+    /**
+     * LListat dels Judicis celebrats al jutjat.
+     */
     private ArrayList<Judici> llistaJudicis;
+    /**
+     * Llistat dels Jutges del Jutjat.
+     */
     private ArrayList<Jutge> llistaJutges;
+    /**
+     * Llistat de les sales que te el Jutjat.
+     */
     private ArrayList<Sala> llistaSales;
 
+    /**
+     * Constructor de la classe.
+     *
+     * @param nom Pasam el nom del jutjat.
+     * @param direccio Pasam la direcció del jutjat.
+     * @param poblacio Pasam la població on es troba el jutjat.
+     */
     public Jutjat(String nom, String direccio, String poblacio) {
         this.nom = nom;
         this.direccio = direccio;
@@ -28,6 +53,9 @@ public class Jutjat {
     /**
      * Afegeix una sala a l'array list, si l'objecte passat es null retorna fals
      * sino retorna true.
+     *
+     * @param nouSala Pasam l'objecte de la nova sala.
+     * @return Retorna true o false si ha anat be l'inserció.
      */
     public Boolean afegirSala(Sala nouSala) {
 
@@ -44,6 +72,9 @@ public class Jutjat {
      * Es pasa per parametre l'index de la Sala a treure, en cas que la sala
      * sigui major o igual a 0 i menor a la grandaria de la llista retornara la
      * Sala en cas contrari retornara null.
+     *
+     * @param index Pasam l'index de la sala que es vol treure.
+     * @return Retorna l'objecte de la Sala cercat o null si no l'ha trobat.
      */
     public Sala treureSala(int index) {
 
@@ -56,6 +87,11 @@ public class Jutjat {
 
     }
 
+    /**
+     * Metode per treure un ArrayList de sala.
+     *
+     * @return Torna un ArrayList de Sales.
+     */
     public ArrayList<Sala> getLlistaSales() {
         return llistaSales;
     }
@@ -63,6 +99,9 @@ public class Jutjat {
     /**
      * Afegeix un Jutge a l'array list, si l'objecte passat es null retorna fals
      * sino retorna true.
+     *
+     * @param nouJutge Pasam l'objecte Jutge per a insertar a l'arraylist.
+     * @return Retorna true o false si ha anat be l'inserció.
      */
     public Boolean afegirJutge(Jutge nouJutge) {
 
@@ -79,6 +118,10 @@ public class Jutjat {
      * Es pasa per parametre l'index del Jutge a treure, en cas que l'index
      * sigui major o igual a 0 i menor a la grandaria de la llista retornara el
      * Jutge en cas contrari retornara null.
+     *
+     * @param index Pasam l'index del Jutge que es vol treure.
+     * @return Torna un objecte Jutge amb el jutge cercat o null si no l'ha
+     * trobat.
      */
     public Jutge treureJutge(int index) {
 
@@ -91,6 +134,11 @@ public class Jutjat {
 
     }
 
+    /**
+     * Metode per treure un ArrayList de Jutges.
+     *
+     * @return Torna un ArrayList de Jutges.
+     */
     public ArrayList<Jutge> getLlistaJutges() {
         return llistaJutges;
     }
@@ -98,6 +146,9 @@ public class Jutjat {
     /**
      * Afegeix un Judici a l'array list, si l'objecte passat es null retorna
      * fals sino retorna true.
+     *
+     * @param nouJudici
+     * @return Retorna true o false si ha anat be l'inserció.
      */
     public Boolean afegirJudici(Judici nouJudici) {
 
@@ -114,6 +165,10 @@ public class Jutjat {
      * Es pasa per parametre l'index del Judici a treure, en cas que l'index
      * sigui major o igual a 0 i menor a la grandaria de la llista retornara el
      * Judic en cas contrari retornara null.
+     *
+     * @param index Pasam l'index del Judici que es vol treure.
+     * @return Torna un objecte Judici amb el judici cercat o null si no l'ha
+     * trobat.
      */
     public Judici treureJudici(int index) {
 
@@ -126,6 +181,11 @@ public class Jutjat {
 
     }
 
+    /**
+     * Metode per treure un ArrayList de Judicis.
+     *
+     * @return Torna un ArrayList de Judicis.
+     */
     public ArrayList<Judici> getLlistaJudicis() {
         return llistaJudicis;
     }
